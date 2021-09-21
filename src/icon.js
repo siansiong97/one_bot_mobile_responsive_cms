eeed996aa4b276312361c44f2aa151167344e5d0
siansiong97 <56067813+siansiong97@users.noreply.github.com>
2021-09-21 22:35:46 +0800

    first init

--- src/icon.js
+++
@@ -1,8 +0,0 @@
-
-
-export const playStoreIcon = "/Social Media/Google Play@3x.png";
-export const appleStoreIcon = "/Social Media/Apple Store @3x.png";
-export const huaweiIcon = "/Social Media/huawei.png";
-export const facebookIcon = "/Social Media/Facebook @3x.png";
-export const instagramIcon = "/Social Media/instagram.png";
-export const youtubeIcon = "/Social Media/youtube.png";
